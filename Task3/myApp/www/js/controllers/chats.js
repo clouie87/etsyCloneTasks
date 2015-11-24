b75d7a6f5dbebfaf082162c40645a299806c0bae
clouie87 <carol.louie@gmail.com>
2015-11-24 11:45:51 -0400

    Task3 Create login page

---
+++ Task3/myApp/www/js/controllers/chats.js
@@ -0,0 +1,21 @@
+'use strict'
+
+app.controller('ChatsCtrl', function($scope, Chats, Products) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+  $scope.image ="http://www.maniacworld.com/images/Tiny-Shark-Socks.jpg";
+  $scope.button = function(product){
+    console.log('button was clicked', product, $scope.image);
+    Products.saveProduct(product, $scope.image);
+  }
+})
---
+++ Task3/myApp/www/js/controllers/login.js
@@ -0,0 +1,7 @@
+'use strict';
+
+app.controller('LoginCtrl', function($scope) {
+  $scope.emailLogin = function () {
+    console.log('button was clicked on login');
+  }
+});
