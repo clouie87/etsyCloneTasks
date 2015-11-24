d15c41a85ff05f7875cbd271df7937372d2c8654
clouie87 <carol.louie@gmail.com>
2015-11-24 12:02:18 -0400

    Creating Popup

--- Task3/myApp/www/js/controllers/login.js
+++ Task3/myApp/www/js/controllers/login.js
@@ -1,7 +1,34 @@
 'use strict';
 
-app.controller('LoginCtrl', function($scope) {
+app.controller('LoginCtrl', function($scope, $state, $ionicPopup) {
   $scope.emailLogin = function () {
     console.log('button was clicked on login');
-  }
+    $scope.user = {};
+    console.log('showing popup');
+
+    // An elaborate, custom popup
+    var myPopup = $ionicPopup.show({
+      templateUrl: 'templates/partials/login.html',
+      title: 'Signin',
+      scope: $scope,
+      buttons: [
+        { text: '<b>Login</b>',
+          type: 'button-energized',
+          onTap: function(user) {
+            user = $scope.user;
+            //Log the user in
+          }
+        },
+        {
+          text: '<b>Register</b>',
+          type: 'button-calm',
+          onTap: function(user) {
+            user = $scope.user;
+            //register the user
+
+          }
+        }
+      ]
+    });
+  };
 });
